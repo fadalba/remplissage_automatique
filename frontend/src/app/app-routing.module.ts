6950ce49c207a6d04bdf51501730264188ec5705
fadal ba <fadalba@gmail.com>
2023-04-17 15:42:17 +0000

    cont

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { HeaderComponent } from './header/header.component';
+import { SystemeComponent } from './systeme/systeme.component';
+import { NavformComponent } from './navform/navform.component';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthuserGuard } from './auth/authuser.guard';
+const routes: Routes = [
+  { path: 'connexion', component: ConnexionComponent, canActivate: [AuthGuard] },
+  { path:'systeme',component: SystemeComponent},
+  { path: 'header', component: HeaderComponent, canActivate: [AuthGuard] },
+   { path: 'formulaire', component:NavformComponent},
 
-const routes: Routes = [];
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SystemeComponent } from './systeme/systeme.component';
 import { HeaderComponent } from './header/header.component';
 import { NavformComponent } from './navform/navform.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {SocketIoModule, SocketIoConfig} from 'ngx-socket-io'
 
+const config: SocketIoConfig= {
+  url: 'http://localhost:3001',
+  options: {
+    transports: ['websocket']
+  }
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +27,12 @@ import { ConnexionComponent } from './connexion/connexion.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+     ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule,
+HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
