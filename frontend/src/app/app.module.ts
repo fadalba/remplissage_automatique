14d1597f6a41b0c6bac9d21e666e6b0ceb1fc4ac
fadal ba <fadalba@gmail.com>
2023-04-17 14:01:58 +0000

    création des composants

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SystemeComponent } from './systeme/systeme.component';
+import { HeaderComponent } from './header/header.component';
+import { NavformComponent } from './navform/navform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SystemeComponent,
+    HeaderComponent,
+    NavformComponent
   ],
   imports: [
     BrowserModule,
