861d8f1fb3b3a703dafed26520670f4607cbdcb2
sen2019elec <sen.elecplomb2019@gmail.com>
2023-04-24 19:11:36 +0200

    ok

--- frontend/src/app/formulaire/formulaire.component.ts
+++ frontend/src/app/formulaire/formulaire.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MustMatch } from '../must-match.validator';
 @Component({
   selector: 'app-formulaire',
   templateUrl: './formulaire.component.html',
   styleUrls: ['./formulaire.component.css']
 })
-export class FormulaireComponent {
+export class FormulaireComponent implements OnInit{
   registerForm!: FormGroup
   submitted = false
+f: any;
 
-  constructor(private formBuilder: FormBuilder, private router: Router ) {
+  constructor(private formBuilder: FormBuilder, private router: Router ) { }
+  ngOnInit() {
+
+   
     this.registerForm = this.formBuilder.group({
       actuelpassword:['', [Validators.required, Validators.minLength(6)]],
       newpassword:['', [Validators.required, Validators.minLength(6)]],
-      confirmation: ['', [Validators.required, Validators.minLength(6)]],
-    })
+      confirmation: ['', [Validators.required]],
+    }, { validator: MustMatch('newPassword', 'confirmation') }
+    );
   
 
 }
+/* get f() { return this.registerForm.controls; } */
 onSubmit() {
   this.submitted = true
 
