e3ab7b6af672a8e8e0ed040f4a0094ee209f6ef8
sen2019elec <sen.elecplomb2019@gmail.com>
2023-04-27 01:23:13 +0200

    ok

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MustMatch } from '../must-match.validator';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,13 +11,31 @@ import { Component,OnInit } from '@angular/core';
 export class SidebarComponent  implements OnInit{
   currentDate = new Date();
   CurrentTime: any;
-  constructor( ) {
+  registerForm!: FormGroup
+  submitted = false
+ 
+  constructor(private formBuilder: FormBuilder, private router: Router  ) {
     setInterval(() => {
       this.CurrentTime = new Date().getHours() + ':' + new Date().getMinutes() + ':'+  new Date().getSeconds()}, + 1);
 console.log(this.currentDate);
 
   }
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      actuelpassword:['', [Validators.required, Validators.minLength(6)]],
+      newpassword:['', [Validators.required, Validators.minLength(6)]],
+      confirmation: ['', [Validators.required]],
+    }, { validator: MustMatch('newpassword', 'confirmation') 
+    }
+    );
   }
-
+ 
+  onSubmit() {
+    this.submitted = true
+  
+    if (this.registerForm.invalid) {
+      return
+    }
+  
+  }
 }
