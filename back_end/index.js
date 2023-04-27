6e2854a287d78fd52305949ac84347c1abb7872f
zoukana <assanediallo192@gmail.com>
2023-04-27 13:19:18 +0000

    rr

--- back_end/index.js
+++ back_end/index.js
@@ -69,7 +69,7 @@ const { log } = require('console');
  var parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' })); 
 
 /* port.pipe(parser); */
-var url = "mongodb+srv://fadalba:Thiaroye44@cluster0.9vbufn8.mongodb.net/test";
+var url = "mongodb+srv://fadalba:Thiaroye44@cluster0.vk1j3ac.mongodb.net/soutenance";
 
 
 var temoin = '0'
