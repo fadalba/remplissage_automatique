af9a38b38d7a81d1066cd9ac7c13e8ced4125bb0
zoukana <assanediallo192@gmail.com>
2023-04-28 10:04:21 +0000

    tt

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { SystemeComponent } from './systeme/systeme.component';
 import { NavformComponent } from './navform/navform.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthuserGuard } from './auth/authuser.guard';
-import { FormulaireComponent } from './formulaire/formulaire.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ParametreComponent } from './parametre/parametre.component';
@@ -17,15 +16,11 @@ const routes: Routes = [
   { path:'systeme',component: SystemeComponent},
   { path: 'header', component: HeaderComponent, canActivate: [AuthGuard] },
 
-  { path: 'form', component: FormulaireComponent },
    { path: 'formulaire', component:NavformComponent, canActivate:[AuthuserGuard]},
      {path: 'dashboard', component:DashboardComponent /*,canActivate:[AuthGuard]*/},
   {path: 'parametre', component:ParametreComponent /*,canActivate:[AuthGuard]*/},
   {path: 'historique', component:HistoriqueComponent /*,canActivate:[AuthGuard]*/},
 
-
-
-   { path: 'formulaire', component:FormulaireComponent},
 
 
 
