b93b45d2ec714b53e8f83487f6058b809297a523
fadal ba <fadalba@gmail.com>
2023-04-28 10:13:32 +0000

    ttt

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,8 +6,6 @@ import { SystemeComponent } from './systeme/systeme.component';
 import { NavformComponent } from './navform/navform.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthuserGuard } from './auth/authuser.guard';
-import { FormulaireComponent } from './formulaire/formulaire.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ParametreComponent } from './parametre/parametre.component';
 import { HistoriqueComponent } from './historique/historique.component';
@@ -16,18 +14,10 @@ const routes: Routes = [
   { path: '', component: ConnexionComponent, canActivate: [AuthGuard] },
   { path:'systeme',component: SystemeComponent},
   { path: 'header', component: HeaderComponent, canActivate: [AuthGuard] },
-
-  { path: 'form', component: FormulaireComponent },
-   { path: 'formulaire', component:NavformComponent, canActivate:[AuthuserGuard]},
-     {path: 'dashboard', component:DashboardComponent /*,canActivate:[AuthGuard]*/},
-  {path: 'parametre', component:ParametreComponent /*,canActivate:[AuthGuard]*/},
-  {path: 'historique', component:HistoriqueComponent /*,canActivate:[AuthGuard]*/},
-
-
-
-   { path: 'formulaire', component:FormulaireComponent},
-
-
+  { path: 'formulaire', component:NavformComponent, canActivate:[AuthuserGuard]},
+  {path: 'dashboard', component:DashboardComponent ,canActivate:[AuthGuard]},
+  {path: 'parametre', component:ParametreComponent ,canActivate:[AuthGuard]},
+  {path: 'historique', component:HistoriqueComponent ,canActivate:[AuthGuard]},
 
 ];
 @NgModule({
