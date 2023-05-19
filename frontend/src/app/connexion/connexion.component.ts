026da207da2765a6ad929cb38b5fdb182694e237
zoukana <assanediallo192@gmail.com>
2023-05-19 17:04:17 +0000

    tt

--- frontend/src/app/connexion/connexion.component.ts
+++ frontend/src/app/connexion/connexion.component.ts
@@ -30,7 +30,7 @@ ngOnInit() {
 
     email:['',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
 
-    password:['',[Validators.required,Validators.minLength(8)]],
+    password:['',[Validators.required,Validators.minLength(6)]],
 
     })
 
